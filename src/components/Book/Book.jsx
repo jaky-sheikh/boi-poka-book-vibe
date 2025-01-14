2b2a1f15315d34e9f36080de08fba9aebf21f517
jaky-sheikh <jaky@eu4m.eu>
2025-01-14 11:27:08 +0600

    ratings

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,7 +1,7 @@
 
 const Book = ({ book }) => {
 
-    const { image, bookName, author } = book;
+    const { image, bookName, author, tags, category } = book;
 
     return (
         <div className="card bg-base-100 w-96 shadow-xl p-6">
@@ -12,14 +12,24 @@ const Book = ({ book }) => {
                     alt={bookName} />
             </figure>
             <div className="card-body">
+                <div className="flex justify-between gap-5">
+                    {
+                        tags.map(tag => <button className="btn btn-xs bg-red-400">{tag}</button>)
+                    }
+                </div>
                 <h2 className="card-title">
                     {bookName}
                     <div className="badge badge-secondary">NEW</div>
                 </h2>
                 <p>By: {author}</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
+                <div className="border-t-2 border-dashed"></div>
+                <div className="card-actions justify-between">
+                    <div className="badge badge-outline">{category}</div>
+                    <div className="rating">
+                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-200" />
+                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-100" />
+                    </div>
                 </div>
             </div>
         </div>
